faa606ac9934cad6996bae15fa63c40fbb5e6601
mcdonagg <mcdonagg@gmail.com>
2017-06-05 00:21:45 -0700

    Removed un-used object keys

--- js/quotes.js
+++ js/quotes.js
@@ -2,27 +2,19 @@
 var quotes = [
   {
     quote: "Some guy hit my fender and I said “be fruitful and multiply” but not in those words.",
-    source: "Woody Allen",
-    citation: "",
-    year: ""
+    source: "Woody Allen"
   },
   {
     quote: "I should like to see any kind of a man, distinguishable from a gorilla, that some good and even pretty woman could not shape a husband out of.",
-    source: "Oliver Wendell Holmes, Sr.",
-    citation: "",
-    year: ""
+    source: "Oliver Wendell Holmes, Sr."
   },
   {
     quote: "When I was born … the doctor came out to the waiting room and said to my father … I’m very sorry. We did everything we could … but he pulled through.",
-    source: "Rodney Dangerfield",
-    citation: "",
-    year: ""
+    source: "Rodney Dangerfield"
   },
   {
     quote: "When you are waiting for the bus and someone asks, “Has the bus come yet?”. If the bus came would I be standing here?",
-    source: "Billy Connoly",
-    citation: "",
-    year: ""
+    source: "Billy Connoly"
   },
   {
     quote: "Badges? We don't need no stinkin' badges!",
