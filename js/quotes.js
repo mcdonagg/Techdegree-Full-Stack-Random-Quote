6dd51eb66e25a9be58fad896485b622664be9a75
mcdonagg <mcdonagg@gmail.com>
2017-06-05 11:49:17 -0700

    added some more quotes and Tags

--- js/quotes.js
+++ js/quotes.js
@@ -2,24 +2,39 @@
 var quotes = [
   {
     quote: "Some guy hit my fender and I said “be fruitful and multiply” but not in those words.",
-    source: "Woody Allen"
+    source: "Woody Allen",
+    tags:['funny','movie']
   },
   {
     quote: "I should like to see any kind of a man, distinguishable from a gorilla, that some good and even pretty woman could not shape a husband out of.",
-    source: "Oliver Wendell Holmes, Sr."
+    source: "Oliver Wendell Holmes, Sr.",
+    tags:['funny']
   },
   {
     quote: "When I was born … the doctor came out to the waiting room and said to my father … I’m very sorry. We did everything we could … but he pulled through.",
-    source: "Rodney Dangerfield"
+    source: "Rodney Dangerfield",
+    tags:['funny']
   },
   {
     quote: "When you are waiting for the bus and someone asks, “Has the bus come yet?”. If the bus came would I be standing here?",
-    source: "Billy Connoly"
+    source: "Billy Connoly",
+    tags:['funny','movie']
   },
   {
     quote: "Badges? We don't need no stinkin' badges!",
     source: "Mexican #2",
     citation: "Blazing Saddles",
-    year: "1974"
+    year: "1974",
+    tags:['funny','movie']
+  },
+  {
+    quote: "Letting go means to come to the realization that some people are a part of your history, but not a part of your destiny.",
+    source: "Steve Maraboli",
+    tags:['motivational','success']
+  },
+  {
+    quote: "If you don't know history, then you don't know anything. You are a leaf that doesn't know it is part of a tree.",
+    source: "Michael Crichton",
+    tags:['history']
   }
 ]
