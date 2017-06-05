28d48fb280dfb5227c86a84212e26deb13f06cd9
mcdonagg <mcdonagg@gmail.com>
2017-06-05 00:01:41 -0700

    First run of the functions

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,24 @@
 function getRandomQuote(){
+  var r;
   //create a random integer between 0 and the size of the quotes array
+  r = Math.floor(Math.random() * (quotes.length));
   //return the quote from that index
+  return quotes[r];
 }
 
 function printQuote() {
+  var quote;
+  var html;
   //call the random quote generator to get the quote
+  quote = getRandomQuote();
   //create the HTML string to show the quote
+  html = '<p class="quote">' + quote.quote + '</p>';
+  html += '<p class="source">' + quote.source;
+  //if citation
+  html += '<span class="citation">' + quote.citation + '</span>';
+  //if year
+  html += '<span class="year">' + quote.year + '</span>';
+  html += '</p>';
   //send that string to the DOM
+  document.getElementById('quote-box').innerHTML = html;
 }
