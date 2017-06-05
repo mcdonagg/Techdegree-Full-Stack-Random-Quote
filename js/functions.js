99e9a77330ac6e7b67ec65353fc8f4b201b88ebc
mcdonagg <mcdonagg@gmail.com>
2017-06-04 23:50:41 -0700

    Setup the files and the outline for the project

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+function getRandomQuote(){
+  //create a random integer between 0 and the size of the quotes array
+  //return the quote from that index
+}
+
+function printQuote() {
+  //call the random quote generator to get the quote
+  //create the HTML string to show the quote
+  //send that string to the DOM
+}
---
+++ js/quotes.js
@@ -0,0 +1,33 @@
+//This file is simply the setup for the variable that will list all the quotes
+var quotes = [
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: ""
+  }
+]
